45c68c7fd673ae18e6ab774f7952e4167d23c778
Matthew Effam <59513871+mattmpe09@users.noreply.github.com>
2024-12-01 14:50:04 +0100

    Add outputs for important resource information
    Defines output values to display important information after deployment, such as public IP addresses and database names.

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,6 @@ output "web_lb_public_ip" {
   description = "Public IP address of the Load Balancer"
   value       = azurerm_public_ip.lb_public_ip.ip_address
 }
-
 output "app_gateway_public_ip" {
   description = "Public IP address of the Application Gateway"
   value       = azurerm_public_ip.appgw_public_ip.ip_address
